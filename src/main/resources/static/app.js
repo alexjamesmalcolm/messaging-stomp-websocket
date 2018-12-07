89d76e1343bf00b78b1c63f8e7c1aa349c9b5806
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2018-12-06 19:21:31 -0500

    Filled app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,12 +1,57 @@
 let stompClient = null;
 
-function setConnected(connected) {}
+const all = true;
 
-function connect() {}
+function $(selector, all) {
+    if (all) {
+        return document.querySelectorAll(selector);
+    }
+    return document.querySelector(selector);
+}
 
-function disconnect() {}
+function setConnected(connected) {
+    $("#connect").setAttribute("disabled", connected);
+    $("#disconnect").setAttribute("disabled", !connected);
+    if(connected) {
+        $("#conversation").classList.remove("hide");
+    } else {
+        $("#conversation").classList.add("hide");
+    }
+    $("#greetings").innerHTML = "";
+}
 
-function sendName() {}
+function connect() {
+    const socket = new SockJs("/gs-guide-websocket");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, frame => {
+        setConnected(true);
+        console.log(`Connected: ${frame}`);
+        stompClient.subscribe("/topic/greetings", greeting => showGreeting(JSON.parse(greeting.body).content));
+    });
+}
 
-function showGreeting(message) {}
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
 
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({
+        "name": $("#name").value
+    }));
+}
+
+function showGreeting(message) {
+    document.querySelector("#greetings").innerHTML += `<tr><td>${message}</td></tr>`;
+}
+
+$("form", all).forEach(form => {
+    form.addEventListener("submit", e => e.preventDefault());
+});
+
+$("#connect").addEventListener("click", e => connect());
+$("#disconnect").addEventListener("click", e => disconnect());
+$("#send").addEventListener("click", e => sendName());
